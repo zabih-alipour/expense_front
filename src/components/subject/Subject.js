458003134bbc1af9e63108cfd73cc5cac04466f2
Alipour <za.alipour.java@gmail.com>
2020-12-16 17:28:00 +0330

    Implement suggestion

--- src/components/subject/Subject.js
+++ src/components/subject/Subject.js
@@ -4,29 +4,49 @@ import './SubjectList'
 import '../invoice/InvoiceList'
 import SubjectList from "./SubjectList";
 import SubjectLineChart from "../report/SubjectLineChart";
-import {Container, Row, Col} from 'react-bootstrap'
+import SubjectAdd from "./SubjectAdd";
+import {Container, Row, Col, Button} from 'react-bootstrap'
 
 class Subject extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            show: false,
             selectedSubject: 1
         }
     }
 
-    filterItem (selectedItem){
+    filterItem(selectedItem) {
         this.setState({
             selectedSubject: selectedItem
         });
     }
 
+    showModal() {
+        this.setState({show: true})
+    }
+
+    closeModal() {
+        this.setState({show: false})
+    }
+
 
     render() {
+        const {show} = this.state
         return (
             <Container fluid={true} className={"mt-2"}>
+                <SubjectAdd show={show}
+                            onHide={() => this.closeModal()}
+                />
+                <Row>
+                    <Col xs={{span: 11}}/>
+                    <Col xs={{span: 1}}>
+                        <Button onClick={() => this.showModal()}>ثبت جدید</Button>
+                    </Col>
+                </Row>
                 <Row>
                     <Col xs={{span: 3}}>
-                        <SubjectList onClick={(p)=>this.filterItem(p)}/>
+                        <SubjectList onClick={(p) => this.filterItem(p)}/>
                     </Col>
                     <Col xs={{span: 9}}>
                         <SubjectLineChart selectedSubject={this.state.selectedSubject}/>
@@ -35,6 +55,8 @@ class Subject extends React.Component {
             </Container>
         )
     }
+
+
 }
 
 export default Subject
--- src/components/subject/SubjectAdd.js
+++ src/components/subject/SubjectAdd.js
@@ -1,13 +1,41 @@
 import React from "react";
 import './SubjectList.css'
-import {Button, Modal} from 'react-bootstrap'
+import {Button, Col, Container, FormControl, InputGroup, Modal, Row} from 'react-bootstrap'
 
 class SubjectAdd extends React.Component {
-    state = {
-        subjects: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            matchedSubjects: [],
+            show: props.show,
+            subject: {
+                name: ""
+            }
+        }
+    }
+
+    saveSubject() {
+
+        this.props.onHide()
+    }
+
+    onTextChange(txt) {
+        if (txt.trim().length > 0) {
+            fetch('/subjects/' + txt)
+                .then(res => res.json())
+                .then(data => {
+                    if (data.length > 0) {
+                        this.setState({matchedSubjects: data})
+                    } else this.setState({matchedSubjects: []})
+                })
+        } else this.setState({matchedSubjects: []})
     }
 
     render() {
+        const cols = []
+        this.state.matchedSubjects.forEach((subject, key) => {
+            cols.push(<Col key={key}> {subject.name} </Col>)
+        })
         return (
             <Modal
                 {...this.props}
@@ -15,21 +43,29 @@ class SubjectAdd extends React.Component {
                 aria-labelledby="contained-modal-title-vcenter"
                 centered
             >
-                <Modal.Header closeButton>
+                <Modal.Header>
                     <Modal.Title id="contained-modal-title-vcenter">
-                        Modal heading
+                        اضافه کردن آیتم جدید
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <h4>Centered Modal</h4>
-                    <p>
-                        Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                        consectetur ac, vestibulum at eros.
-                    </p>
+                    <InputGroup className="mb-3">
+                        <FormControl onChange={(e) => this.onTextChange(e.target.value)}
+                                     type={"text"}
+                                     placeholder="نام آیتم خرید را وارد کنید"
+                                     aria-label="نام آیتم"
+                                     aria-describedby="basic-addon2"
+                        />
+                    </InputGroup>
+                    <Container>
+                        <Row>
+                            {cols}
+                        </Row>
+                    </Container>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button >Close</Button>
+                    <Button onClick={() => this.props.onHide()}>بستن</Button>
+                    <Button onClick={() => this.saveSubject()}>ذخیره</Button>
                 </Modal.Footer>
             </Modal>
         )

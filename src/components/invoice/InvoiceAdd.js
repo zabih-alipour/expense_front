e321dc2bdb79f221df2b6adfd110adca4e04edb3
Alipour <za.alipour.java@gmail.com>
2020-12-16 22:38:29 +0330

    Add invoice creation

--- src/components/invoice/InvoiceAdd.js
+++ src/components/invoice/InvoiceAdd.js
@@ -1,23 +1,85 @@
 import React from "react";
-import './InvoiceList.css'
-import {Container, Row, Col} from 'react-bootstrap'
+import {Button, Form, Modal} from 'react-bootstrap'
 
-class InvoiceAdd extends React.Component {
-    state = {
-        invoice: []
+function InvoiceAdd(props) {
+    const subjectEL = React.useRef(null)
+    const factorDateEL = React.useRef(null)
+    const priceEL = React.useRef(null)
+    const quantityEL = React.useRef(null)
+    const descriptionEL = React.useRef(null)
+
+    function saveInvoice(event) {
+        event.preventDefault()
+        const invoice = {
+            factor_date: factorDateEL.current.value,
+            item: {
+                id: null,
+                name: subjectEL.current.value
+            },
+            price: priceEL.current.value,
+            quantity: quantityEL.current.value,
+            description: descriptionEL.current.value
+        }
+
+        fetch('/invoices', {
+            method: 'post',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(invoice)
+        })
+            .then(res => res.json())
+            .then(data => {
+
+            })
     }
 
-    render() {
-        return (
-            <Container>
-                <Row>
-                    <Col>
 
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Form className="mb-3" onSubmit={(event) => saveInvoice(event)}>
+                <Modal.Header>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        اضافه کردن فاکتور جدید
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+
+                    <Form.Group controlId="item">
+                        <Form.Control type="text" placeholder="آیتم خریداری شده"
+                                      ref={subjectEL}/>
+                    </Form.Group>
+                    <Form.Group controlId="factor_date">
+                        <Form.Control type="number" placeholder="تاریخ"
+                                      ref={factorDateEL}/>
+                    </Form.Group>
+                    <Form.Group controlId="quantity">
+                        <Form.Control type="number" placeholder="تعداد"
+                                      ref={quantityEL}/>
+                    </Form.Group>
+                    <Form.Group controlId="price">
+                        <Form.Control type="number" placeholder="قیمت"
+                                      ref={priceEL}/>
+                    </Form.Group>
+                    <Form.Group controlId="description">
+                        <Form.Control as="textarea" rows={3} placeholder="توضیحات"
+                                      ref={descriptionEL}/>
+                    </Form.Group>
+
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={() => this.props.onHide()}>بستن</Button>
+                    <Button type={"submit"}>ذخیره</Button>
+                </Modal.Footer>
+            </Form>
+        </Modal>
+    )
+
+
 }
 
 export default InvoiceAdd
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,8 +1,22 @@
 import React from "react";
 import './Menu.css'
-import {Navbar, Nav} from 'react-bootstrap'
+import '../invoice/InvoiceAdd'
+import {Navbar, Nav, Button} from 'react-bootstrap'
+import InvoiceAdd from "../invoice/InvoiceAdd";
 
 class Menu extends React.Component {
+
+    state = {
+        show: false
+    }
+
+    showModal() {
+        this.setState({show: true})
+    }
+
+    closeModal() {
+        this.setState({show: false})
+    }
 
     render() {
         return (
@@ -15,19 +29,27 @@ class Menu extends React.Component {
                         onSelect={(selectedKey) => this.props.onClick(selectedKey)}
                     >
                         <Nav.Item>
-                            <Nav.Link eventKey={"home"} >خانه</Nav.Link>
+                            <Nav.Link eventKey={"home"}>خانه</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
                             <Nav.Link eventKey={"subject"}>اقلام خرید</Nav.Link>
                         </Nav.Item><Nav.Item>
-                            <Nav.Link eventKey={"invoice"}>صورت حساب</Nav.Link>
-                        </Nav.Item>
+                        <Nav.Link eventKey={"invoice"}>صورت حساب</Nav.Link>
+                    </Nav.Item>
                         <Nav.Item>
                             <Nav.Link eventKey={"report"}>گزارش</Nav.Link>
                         </Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
+                <Nav.Item>
+                    <Button variant="outline-light"
+                            onClick={() => this.showModal()}>فاکتور جدید</Button>
+                    <InvoiceAdd
+                        show={this.state.show}
+                        onHide={() => this.closeModal()}/>
+                </Nav.Item>
             </Navbar>
+
         )
     }
 }

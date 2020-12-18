ec0ba05bd1cd01f30e6ad015b18c5e2be44fd5b8
Alipour <za.alipour.java@gmail.com>
2020-12-18 16:25:57 +0330

    Add local search to subjects

--- src/components/subject/SubjectList.js
+++ src/components/subject/SubjectList.js
@@ -1,28 +1,45 @@
 import React from "react";
 import './SubjectList.css'
-import {Container, ListGroup} from 'react-bootstrap'
+import {Container, Form, ListGroup} from 'react-bootstrap'
 
 class SubjectList extends React.Component {
     state = {
-        subjects: []
+        subjects: [],
+        searchSubjects: []
     }
 
     componentDidMount() {
         fetch('/subjects')
             .then(res => res.json())
             .then(data => {
-                this.setState({subjects: data});
+                this.setState({
+                    subjects: data,
+                    searchSubjects: data,
+                });
             });
+    }
+
+    onSearch(val) {
+        if (val !== undefined && val !== null && val.trim().length > 0) {
+            let searchSubjects = this.state.subjects.filter(p => p.name.slice(0, val.length) === val);
+            console.log(searchSubjects)
+            this.setState({searchSubjects: searchSubjects})
+        } else this.setState({searchSubjects: this.state.subjects})
     }
 
     render() {
         return (
             <Container className={"scroll-300"}>
-                <ListGroup  onSelect={(selectedKey)=> this.props.onClick(selectedKey)}>
+                <ListGroup onSelect={(selectedKey) => this.props.onClick(selectedKey)}>
+                    <ListGroup.Item style={{backgroundColor: 'lightsteelblue'}}>
+                        <Form.Control type={"text"} placeholder={"برای جستجو تایپ کنید"}
+                                      onChange={(e) => this.onSearch(e.target.value)}/>
+                    </ListGroup.Item>
+
                     {
-                        this.state.subjects.map((data, key) => {
+                        this.state.searchSubjects.map((data, key) => {
                             return (
-                                <ListGroup.Item key={key} action eventKey={data.id}>
+                                <ListGroup.Item className={"subject-list-item"} key={key} action eventKey={data.id}>
                                     {data.name}
                                 </ListGroup.Item>
                             )
